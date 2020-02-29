2881d737156b78e668b4404accf7b144af011e14
simpleauthority <jacob@algorithmjunkie.com>
2020-02-29 15:20:47 -0800

    will this work lol

--- src/pages/AskUsAnything.vue
+++ src/pages/AskUsAnything.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Ask us Anything...</h1>
+    <iframe scrolling="no" src="https://www.tumblr.com/ask_form/thisisatestbloggerino.tumblr.com" style="background-color: transparent; overflow: hidden; height: 190px;" id="ask_form" width="100%" height="190" frameborder="0"></iframe>
   </div>
 </template>
 
--- src/pages/SubmitAPost.vue
+++ src/pages/SubmitAPost.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Submit a Post...</h1>
+    <iframe scrolling="no" id="submit_form" src="https://www.tumblr.com/submit_form/thisisatestbloggerino.tumblr.com" style="background-color:transparent; overflow:hidden;" width="100%" height="530" frameborder="0"></iframe>
   </div>
 </template>
 
