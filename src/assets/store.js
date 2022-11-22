46f4b46d9b0dc03056730c724c7b4565c306f7ee
Jacob Andersen <jacob@algorithmjunkie.com>
2022-11-22 14:56:59 -0800

    oops. comment dummy

--- src/assets/store.js
+++ src/assets/store.js
@@ -3,10 +3,10 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-import dummy from "../dummy";
+// import dummy from "../dummy";
 
 export default new Vuex.Store({
   state: {
-    blog: /*window.blog || {} */ dummy,
+    blog: window.blog || {} /*dummy*/,
   },
 });
